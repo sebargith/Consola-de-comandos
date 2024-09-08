5912140f4c9f641ef958277845f6f91c9c066e20
sebargith <131008747+sebargith@users.noreply.github.com>
2024-09-08 17:15:16 -0300

    Se agrega fork() y execvp(), para manejar procesos
    EL comando se ejecutará dentro del procesos hijo, el padre usará la funcionalidad wait() para esperar al hijo

--- Shell.c
+++ Shell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define T_MAX 1024  //tamaño maximo de entrada
 #define T_MAX_ARG 100  //Tamaño maximo de argumentos
@@ -11,27 +12,29 @@ void shell() {
     char *args[T_MAX_ARG];
     char *temp;
     int i;
+    pid_t pid; // Para almacenar el ID del proceso hijo
+    int estado; // Para almacenar el estado de terminación del hijo
 
     while (1) {
-        //Mostramos el propmt de la shell
+        // 1. Mostrar el prompt de la shell
         printf("TestShell> ");
         fflush(stdout);
 
-        //Leemos el argumento
+        // 2. Leer el argumento
         if (fgets(input, T_MAX, stdin) == NULL) {
             perror("Error leyendo la entrada");
             continue;
         }
 
-        //Salto de linea
+        // Remover el salto de línea de la entrada
         input[strcspn(input, "\n")] = '\0';
 
-        //Para continuar en caso de tener enter como entrada
+        // Continuar si se presiona "enter" sin escribir un comando
         if (strlen(input) == 0) {
             continue;
         }
 
-        // Guardamos el argumento
+        // Guardar el comando y los argumentos
         i = 0;
         temp = strtok(input, " ");
         while (temp != NULL) {
@@ -39,12 +42,23 @@ void shell() {
             i++;
             temp = strtok(NULL, " ");
         }
-        args[i] = NULL;
+        args[i] = NULL; // El último argumento debe ser NULL para execvp
 
-        // Procesamos el argumento de forma inicial devolviendo la entrada
-        printf("Comando: %s\n", args[0]);
-        for (int j = 1; j < i; j++) {
-            printf("Argumento %d: %s\n", j, args[j]);
+        //Creamos un proceso hijo para ejecutar el comando
+        pid = fork();
+
+        if (pid == -1) {
+            //Caso de error
+            perror("Error al crear el proceso hijo");
+        } else if (pid == 0) {
+            // Proceso hijo: ejecutar el comando
+            if (execvp(args[0], args) == -1) {
+                perror("Error ejecutando el comando");
+            }
+            exit(EXIT_FAILURE); // Terminar el proceso hijo en caso de error
+        } else {
+            // Proceso padre: esperar a que el hijo termine
+            wait(&estado); 
         }
     }
 }
@@ -52,6 +66,5 @@ void shell() {
 int main() {
 
     shell();
-
     return 0;
 }
